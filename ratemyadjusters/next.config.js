63f935608c726b0c293dfc02167bb2cdbccbf521
RateMyAdjusters <mail@adminpostbox.com>
2025-12-11 20:15:46 -0500

    Update next.config.js

--- ratemyadjusters/next.config.js
+++ ratemyadjusters/next.config.js
@@ -11,3 +11,22 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+async redirects() {
+  return [
+    {
+      source: '/how-can-i-review-my-insurance-adjuster',
+      destination: '/guides/how-to-review-your-insurance-adjuster',
+      permanent: true,
+    },
+    {
+      source: '/review-my-adjuster',
+      destination: '/guides/how-to-review-your-insurance-adjuster',
+      permanent: true,
+    },
+    {
+      source: '/rate-my-adjuster',
+      destination: '/guides/how-to-review-your-insurance-adjuster',
+      permanent: true,
+    },
+  ]
+}
